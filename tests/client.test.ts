d252abbc51c851c8a4728c53e6b0370026e3164b
czanella <cfszanella@gmail.com>
2024-12-26 09:21:02 +0000

    Add test for signature checking. This test is also in rust.
    Remove the failing test from the integration tests

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,6 +1,8 @@
-import { describe, expect, jest, test } from "@jest/globals";
-import { TurbineClient } from "../src/turbineClient";
-import { ORDER_INTENT, WALLET_CLIENT } from "./constants";
+import { describe, expect, jest } from "@jest/globals";
+import { convertSignature, TurbineClient } from "../src/turbineClient";
+import { ACCOUNT, ORDER_INTENT, WALLET_CLIENT } from "./constants";
+import { OrderIntent, PrimitiveSignature } from "../src/models";
+import { NULL_ADDRESS, USDC, USDT } from "../src/constants";
 
 describe("TurbineClient", () => {
     describe("addOrder", () => {
@@ -51,5 +53,40 @@ describe("TurbineClient", () => {
             expect(error.message).toMatch(/Failed to parse response as JSON/);
             expect(error.message).toMatch(/happy chrysler/);
         });
+    });
+
+    it("should successfully sign an order intent", async () => {
+        const turbineClient = new TurbineClient();
+        // Define order here to avoid signature invalidation in case
+        // we ever change the order defined in test constants.
+        const orderIntent: OrderIntent = {
+            owner: ACCOUNT.address,
+            sellToken: USDC.address,
+            buyToken: USDT.address,
+            sellAmount: 1000n,
+            minBuyAmount: 950n,
+            midPriceDelta: 5,
+            startTime: 1630000000n,
+            endTime: 1630003600n,
+            partialFill: true,
+            callData: "0x",
+            callDataTarget: NULL_ADDRESS,
+            salt: "0xbc99a2cb0a86c1eb704c1b670ec4c59eae55ceaa8f1b0068f170d6d66d1301a1",
+        } as const;
+
+        const signature = await turbineClient["signIntent"](orderIntent, WALLET_CLIENT);
+
+        const convertedSignature = convertSignature(signature);
+        // The values below are taken from Rust implementation
+        const expected: PrimitiveSignature = {
+            r: BigInt(
+                "0xb4584666ee9c235f6f86cb6196ed9b0b2005c803252f6a3c2cf7ee970bca543b"
+            ),
+            s: BigInt(
+                "0x0f5b2f34a280c43653405b040b9642cf680894a95ca579373f3e359728261183"
+            ),
+            yParity: "0x1",
+        };
+        expect(convertedSignature).toEqual(expected);
     });
 });
--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -1,46 +1,10 @@
-import { NULL_ADDRESS, USDC, USDT } from "../src/constants";
-import { OrderIntent, PrimitiveSignature } from "../src/models";
-import { convertSignature, getRandomSalt, TurbineClient } from "../src/turbineClient";
-import { ACCOUNT, ORDER_INTENT, WALLET_CLIENT } from "./constants";
+import { OrderIntent } from "../src/models";
+import { getRandomSalt, TurbineClient } from "../src/turbineClient";
+import { ORDER_INTENT, WALLET_CLIENT } from "./constants";
 
 describe("Integration test", () => {
-    it("should successfully sign an order intent", async () => {
-        const turbineClient = new TurbineClient();
-        // Define order here to avoid signature invalidation in case
-        // we ever change the order defined in test constants.
-        const orderIntent: OrderIntent = {
-            owner: ACCOUNT.address,
-            sellToken: USDC.address,
-            buyToken: USDT.address,
-            sellAmount: 1000n,
-            minBuyAmount: 950n,
-            midPriceDelta: 5,
-            startTime: 1630000000n,
-            endTime: 1630003600n,
-            partialFill: true,
-            callData: "0x",
-            callDataTarget: NULL_ADDRESS,
-            salt: "0xbc99a2cb0a86c1eb704c1b670ec4c59eae55ceaa8f1b0068f170d6d66d1301a1",
-        } as const;
-
-        const signature = await turbineClient["signIntent"](orderIntent, WALLET_CLIENT);
-
-        const convertedSignature = convertSignature(signature);
-        // The values below are taken from Rust implementation
-        const expected: PrimitiveSignature = {
-            r: BigInt(
-                "0xc3671a04e7b7d275b29cd2e431581700049c50fe29f85ed6f3311c497f71a4eb"
-            ),
-            s: BigInt(
-                "0x5922f8c8f199d220a92a5937e7a8da7448711442ae16a335b58107984b26c618"
-            ),
-            yParity: "0x1",
-        };
-        expect(convertedSignature).toEqual(expected);
-    });
-
     it("should successfully submit an order", async () => {
-        const turbineClient = new TurbineClient();
+        const turbineClient = new TurbineClient("http://0.0.0.0:8080");
 
         await turbineClient.addOrder(ORDER_INTENT, WALLET_CLIENT);
     });
