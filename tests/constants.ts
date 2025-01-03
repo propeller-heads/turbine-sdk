48fea45240e7c5ffc545135b25b9ca7184291173
Michał D <michal@propellerheads.xyz>
2025-01-03 13:21:13 +0100

    feat: Use RPC URL from env var in test wallet client
    Public RPC causes issues sometimes.

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,10 +1,10 @@
-import { Address, createWalletClient, Hex, http } from "viem";
+import { createWalletClient, Hex, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
+import { RPC_URL } from "../src/config";
 import { NULL_ADDRESS, USDC, USDT } from "../src/constants";
 import { OrderIntent } from "../src/models";
 import { mainnet } from "viem/chains";
 
-export const PREFUNDED_ADDRESS: Address = "0xBE69d72ca5f88aCba033a063dF5DBe43a4148De0";
 export const PREFUNDED_PK: Hex =
     "0x91ab9a7e53c220e6210460b65a7a3bb2ca181412a8a7b43ff336b3df1737ce12";
 export const USDC_SELLER_PK: Hex =
@@ -15,7 +15,7 @@ export const ACCOUNT = privateKeyToAccount(PREFUNDED_PK);
 export const WALLET_CLIENT = createWalletClient({
     account: ACCOUNT,
     chain: mainnet,
-    transport: http(),
+    transport: http(RPC_URL),
 });
 
 export const ORDER_INTENT: OrderIntent = {
