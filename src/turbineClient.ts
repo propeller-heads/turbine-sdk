9968c387a1aced3b7031fe0a52b766a54b29336d
Michał D <michal@propellerheads.xyz>
2025-07-22 20:10:15 +0200

    fix: Increase liquidity action validity
    It was outdated and too short.

--- src/turbineClient.ts
+++ src/turbineClient.ts
@@ -582,8 +582,7 @@ export class TurbineClient {
     ): Promise<AddLiquidity> {
         let intentSignature = await this.signIntent(intent, walletClient);
 
-        // At least one block time + speedbump (16 seconds) and at most two blocks time (24 seconds)
-        let deadline = BigInt(Math.floor(Date.now() / 1000) + 20); // 20 seconds from now
+        let deadline = BigInt(Math.floor(Date.now() / 1000) + 24); // 24 seconds from now
         let { permit: permit0, permitSignature: permitSignature0 } =
             await getSignedAllowance({
                 token: intent.token0,
